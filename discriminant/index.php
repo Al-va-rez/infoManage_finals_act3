c5e39ca738604d63222d1a463e1214eb0570a90d
Al-va-rez <167593484+Al-va-rez@users.noreply.github.com>
2024-05-05 16:07:47 +0800

    Update index.php

--- discriminant/index.php
+++ discriminant/index.php
@@ -31,18 +31,19 @@
             <p><i>b^2 - 4ac</i></p>
 
             <form action="index.php" method="POST">
-                <p>
+                <p> <!-- A -->
                     <label for="value_a">A: </label>
                     <input type="text" id="value_a" placeholder="Value of a" name="input_a">
                 </p>
-                <p>
+                <p> <!-- B -->
                     <label for="value_b">B: </label>
                     <input type="text" id="value_b" placeholder="Value of b" name="input_b">
                 </p>
-                <p>
+                <p> <!-- C -->
                     <label for="value_c">C: </label>
                     <input type="text" id="value_c" placeholder="Value of c" name="input_c">
                 </p>
+                
                 <p><input type="submit" value="Submit" name="submit_values"></p>
             </form>
 
@@ -72,7 +73,7 @@
                         echo '<h2>' . $b_value . '^2 - 4(' . $a_value . ')(' . $c_value. ')</h2>';
                         echo '<h2>The answer is ' . $result . '</h2>';
                     }
-                    // check if a text fields remaines empty
+                    // check if a text field remaines empty
                     else if ($_POST['input_a'] == '' || $_POST['input_b'] == '' || $_POST['input_c'] == '') {
                         echo '<h2>THERE ARE MISSING VALUES</h2>';
                     }
